e7800b6c5b8ef69058193ce2ee8df8b39d1bbcd0
chenqianwen <1433414364@qq.com>
2019-03-13 14:39:18 +0800

    readme

---
+++ redis-slave/start.sh
@@ -0,0 +1,23 @@
+cd /redis
+redis_role=$1
+echo $redis_role
+if [ $redis_role = "master" ] ; then
+    echo "master" 
+    sed -i "s/\$redis_port/$redis_port/g" redis.conf
+    redis-server /redis/redis.conf
+elif [ $redis_role = "slave" ] ; then   
+    echo "slave" 
+    sed -i "s/\$redis_port/$redis_port/g" redis.conf
+    sed -i "s/#slaveof/slaveof/g" redis.conf
+    sed -i "s/\$master_ip/$master_ip/g" redis.conf
+    sed -i "s/\$master_port/$master_port/g" redis.conf
+    redis-server /redis/redis.conf
+elif [ $redis_role = "sentinel" ] ; then 
+    echo "sentinel" 
+    sed -i "s/\$sentinel_port/$sentinel_port/g" sentinel.conf
+    sed -i "s/\$master_ip/$master_ip/g" sentinel.conf
+    sed -i "s/\$master_port/$master_port/g" sentinel.conf
+    redis-sentinel /redis/sentinel.conf
+else 
+    echo "unknow role!" 
+fi     #ifend
